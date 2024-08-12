ff143ee5b43d578610d26950e1da3269d8f79e2f
Nyein Chann Moe <nyeinchann2001moe@gmail.com>
2024-08-12 22:17:23 +0630

    add new

---
+++ KoiCoi.Database/AppDbContextModels/EventFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KoiCoi.Database.AppDbContextModels;
+
+public partial class EventFile
+{
+    public int UrlId { get; set; }
+
+    public string? Url { get; set; }
+
+    public string? UrlDescription { get; set; }
+
+    public int EventId { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public string? Extension { get; set; }
+}
